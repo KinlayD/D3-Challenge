a4f0e379c0df48a8af91398448e7f26d722d26c7
KinlayDenning <89169906+KinlayD@users.noreply.github.com>
2022-09-15 15:53:21 +1000

    added changes to app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,15 +1,15 @@
 // conda activate bootcamp -> python -m http.server
 
 // define the svg area dimensions
-var svgWidthScatter = 960;
-var svgHeightScatter = 500;
+var svgWidthScatter = 700;
+var svgHeightScatter = 600;
 
 // define the chart's margins
 var chartMarginScatter = {
-  top: 20,
+  top: 40,
   right: 40,
-  bottom: 80,
-  left: 100
+  bottom: 40,
+  left: 40
 };
 
 // define the chart area within the svg element, minus the margins
@@ -35,12 +35,12 @@ d3.csv("data/data.csv").then(function(data) {
 
     // configure x scale function
     var xScale = d3.scaleLinear()
-        .domain([0, d3.max(data, element => element.poverty)])
+        .domain([d3.min(data, element => element.poverty) - 2, d3.max(data, element => element.poverty) + 2])
         .range([0, chartWidthScatter]);
 
     // configure y scale function
     var yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, element => element.healthcare)])
+        .domain([d3.min(data, element => element.healthcare) - 2, d3.max(data, element => element.healthcare) + 2])
         .range([chartHeightScatter, 0]);
     
     // configure x axis function
@@ -58,6 +58,7 @@ d3.csv("data/data.csv").then(function(data) {
     chartGroupScatter.append("g")
         .call(yAxis);
     
+    var radius = 10;
     // append circles to chart group
     var circlesGroup = chartGroupScatter.selectAll("circle")
         .data(data)
@@ -65,15 +66,39 @@ d3.csv("data/data.csv").then(function(data) {
         .append("circle")
         .attr("cx", element => xScale(element.poverty))
         .attr("cy", element => yScale(element.healthcare))
-        .attr("class", "stateCircle")
-        .attr("r", "10");
+        .classed("stateCircle", true)
+        .attr("r", radius);
     
-    // append text to circles
-    circlesGroup.append("text")
+    var textGroupScatter = chartGroupScatter.append("g");
+
+    textGroupScatter.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
         .text(function(element) {return (element.abbr)})
-        .attr("class", "stateText");
+        .attr("x", element => xScale(element.poverty))
+        .attr("y", element => yScale(element.healthcare))
+        .attr("font-size", radius / ((radius * 10) / 100))
+        .attr("dy", radius / ((radius * 25) / 100))
+        .classed("stateText", true);
+    
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([40, -60])
+        .html(function(element) {
+            return (`state: ${element.state}<br> 
+            population with no healthcare: ${element.healthcare}%<br>
+            population in poverty: ${element.poverty}%`);
+        });
+    
+    circlesGroup.call(toolTip);
 
-
+    circlesGroup.on("mouseover", function(element) {
+        toolTip.show(element, this);
+    })
+    .on("mouseout", function(element) {
+        toolTip.hide(element);
+    })
 
     }).catch(function(error) {
         console.log(error);
